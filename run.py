82daba064ea4acf692445f989bfb70a23772cb24
cmd-source <conorcrosbie09@gmail.com>
2021-05-30 09:18:36 +0000

    adding in a new function to calculate the surplus stock

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def get_sales_data():
 
         sales_data = data_str.split(",")
         validate_data(sales_data)
-    
+
         if validate_data(sales_data):
             print("Yay data is valid!")
             break
@@ -48,17 +48,35 @@ def validate_data(values):
         return False
     return True
 
+
 def update_sales_worksheet(data):
     """
-    Function updates a new line od data into the Googlesheets
+    Function updates a new line of data into the Googlesheets
     """
     print("Updating sales worksheet...")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    Function used with sales and stock data to calculate the surplus data
+    """
+    print("calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
 
 
+def main(): 
+    """
+    Run all program functions
+    """
+  
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("welcome to LoveSandwiches")
+main()
