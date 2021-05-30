70bb3bf8d18266600b67870d706d2ad4ccebd3e2
cmd-source <conorcrosbie09@gmail.com>
2021-05-30 09:50:06 +0000

    adding in the the array and zip method to irrate through both lists and deliver the surplus data

--- run.py
+++ run.py
@@ -65,7 +65,12 @@ def calculate_surplus_data(sales_row):
     print("calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock,sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    return surplus_data
 
 
 def main(): 
@@ -76,7 +81,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 print("welcome to LoveSandwiches")
 main()
